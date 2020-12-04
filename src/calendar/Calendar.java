f9fa70207187f562a12b8372023b09646fbf70b2
tonic523 <59171113+tonic523@users.noreply.github.com>
2020-12-04 23:40:46 +0900

    Update to the calendar that considers the day of the week

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -2,13 +2,14 @@ package calendar;
 
 public class Calendar {
 
-	public static int[] MONTH_OF_MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+	public static int[] MONTH_OF_MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public Calendar() {
-		
+
 	}
-	public void printCalendar(int year, int month) {
-		//윤년 구하기
+
+	public void printCalendar(int year, int month, int week) {
+		// 윤년 구하기
 		int february = 28;
 		if (year % 4 == 0) {
 			february = 29;
@@ -20,20 +21,23 @@ public class Calendar {
 			}
 		}
 		MONTH_OF_MAX_DAYS[1] = february;
-		
+
 		// 달력 출력
 		System.out.printf("     <<%4d %2d>>", year, month);
 		System.out.println();
-		System.out.println(" SU MO TU WE TH FR SA");
-		System.out.println(" ---------------------");
+		System.out.println("SU\tMO\tTU\tWE\tTH\tFR\tSA");
+		System.out.println(" -------------------------------------------------");
+		for (int i = 0; i < week; i++) {
+			System.out.printf("\t");
+		}
 		for (int i = 1; i <= MONTH_OF_MAX_DAYS[month - 1]; i++) {
-			System.out.printf("%3d",i);
-			if (i % 7 == 0) {
+			System.out.printf(i + "\t");
+			if ((i + week) % 7 == 0) {
 				System.out.println();
 			}
-		}
-		System.out.println();
-}
 
+		}
+
+	}
 
 }
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -3,36 +3,70 @@ package calendar;
 import java.util.Scanner;
 
 public class Prompt {
+	/**요일 계산기
+	 * 		
+	 * @param week
+	 * @return (0,sunday) , (6,saturday)
+	 */
+	public static int parseDay(String week) {
+		if (week.equals("su"))
+			return 0;
+		else if (week.equals("mo"))
+			return 1;
+		else if (week.equals("tu"))
+			return 2;
+		else if (week.equals("we"))
+			return 3;
+		else if (week.equals("th"))
+			return 4;
+		else if (week.equals("fr"))
+			return 5;
+		else if (week.equals("sa"))
+			return 6;
+		else
+			return 0;
 
-	public static void main(String[] args) {	
+	}
+
+	public static void main(String[] args) {
+		int year = 2020;
+		int month = 1;
+		int week = 0;
 
 		Calendar cal = new Calendar();
-		Scanner scanner = new Scanner(System.in);	
-		while(true) {
-			//연도 입력
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			// 연도 입력
 			System.out.println("연도를 입력하세요.");
 			System.out.printf("YEAR> ");
-			int year = scanner.nextInt();
-			
-			//월 입력
+			year = scanner.nextInt();
+
+			// 월 입력
 			System.out.println("월을 입력하세요.(종료 = -1)");
 			System.out.printf("MONTH> ");
-			int month = scanner.nextInt();
+			month = scanner.nextInt();
+
+			// 시작하는 요일 입력
+			System.out.println("첫번째 요일을 입력하세요. (su mo tu we th fr sa)");
+			System.out.printf("WEEKDAY> ");
+			String weekday = scanner.next();
+			week = parseDay(weekday);
 			
-			//예외 처리
-			if(month == -1) {
+			// 예외 처리
+			if (month == -1) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
-			}else if (month < 1 | month > 12) {
-				System.out.println("잘못 입력하였습니다. 다시 입력해주세요!");
+			} else if (month < 1 | month > 12) {
+				System.out.println("월을 잘못 입력하였습니다. 다시 입력해주세요!");
 				continue;
 			}
-			
-			//달력 출력
-			cal.printCalendar(year, month);
-			
+
+			// 달력 출력
+			cal.printCalendar(year, month, week);
+			System.out.println("");
 		}
 		scanner.close();
+
 	}
 
 }
