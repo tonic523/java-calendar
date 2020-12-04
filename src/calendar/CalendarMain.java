2405eabd10e182ad2bc7eecf6c060959c7b78d2c
tonic523 <59171113+tonic523@users.noreply.github.com>
2020-12-04 17:38:45 +0900

    Update repeatable calendar

--- src/calendar/CalendarMain.java
+++ src/calendar/CalendarMain.java
@@ -6,11 +6,11 @@ public class CalendarMain {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public void getMaxDaysOfMonth(int month) {
+	public static void getMaxDaysOfMonth(int month) {
 		System.out.printf("%d월은 %d일까지 있습니다.\n", month, MAX_DAYS[month - 1]);
 	}
 
-	public void printSampleCalender() {
+	public static void printSampleCalender() {
 		System.out.println("  일   월    화    수    목    금    토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
@@ -18,21 +18,23 @@ public class CalendarMain {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("달을 입력하세요.");
-		int month = scanner.nextInt();
-		CalendarMain cal = new CalendarMain();
-
-		if (month < 1 | month > 12) {
-			System.out.println("잘못 입력하였습니다.");
-		} else {
-			cal.getMaxDaysOfMonth(month);
+		System.out.println("반복횟수를 입력하세요.");
+		int num = scanner.nextInt();
+		
+		System.out.println("월을 입력하세요.");
+		int[] month = new int[num];
+		
+		for (int i = 0; i < month.length; i++) {
+			month[i] = scanner.nextInt();
 		}
-		cal.printSampleCalender();
+		for (int j = 0; j < month.length; j++) {
+			getMaxDaysOfMonth(month[j]);
+		}
+		printSampleCalender();
 		scanner.close();
-
 	}
 
 }
