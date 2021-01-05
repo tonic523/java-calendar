c19bab9e9556d140923ee89ef08ae37bdb5f98a9
tonic523 <59171113+tonic523@users.noreply.github.com>
2021-01-05 17:37:46 +0900

    Update Calendar2.java , Prompt.java
    Added a function to display the day with a schedule in Calendar2, and implemented a command in Prompt as a function.

--- src/calendar/Calendar2.java
+++ src/calendar/Calendar2.java
@@ -8,7 +8,7 @@ public class Calendar2 {
 
 	}
 
-	public static int getLeapdays(int year) {
+	public static void getLeapdays(int year) {
 		int february = 28;
 		if (year % 4 == 0) {
 			february = 29;
@@ -19,13 +19,13 @@ public class Calendar2 {
 				}
 			}
 		}
-		return february;
+		MONTH_OF_MAX_DAYS[1] = february;
 	}
-	
+
 	public static int getWeekday(int year, int month) {
-		year --;
+		year--;
 		int week = 1;
-		week = 1 + ((year + year/4 - year/100 + year/400) % 7);
+		week = 1 + ((year + year / 4 - year / 100 + year / 400) % 7);
 		if (month != 1) {
 			for (int i = 0; i < month - 1; i++) {
 				week = (week + (Calendar2.MONTH_OF_MAX_DAYS[i] % 7)) % 7;
@@ -33,14 +33,17 @@ public class Calendar2 {
 		}
 		return week;
 	}
-	
-	public static void printCalendar(int year, int month) {	
-		MONTH_OF_MAX_DAYS[1] = getLeapdays(year); // 윤년 계산
-		int week = getWeekday(year, month); // 1일 요일 계산
+
+	public static void calendarOutline(int year, int month) {
 		System.out.printf("     <<%4d %2d>>", year, month);
 		System.out.println();
 		System.out.println("SU\tMO\tTU\tWE\tTH\tFR\tSA");
 		System.out.println(" -------------------------------------------------");
+	}
+	public static void printCalendar(int year, int month) {
+		getLeapdays(year); // 윤년 계산
+		int week = getWeekday(year, month); // 1일 요일 계산
+		calendarOutline(year, month);
 		for (int i = 0; i < week; i++) {
 			System.out.printf("  \t");
 		}
@@ -50,6 +53,36 @@ public class Calendar2 {
 				System.out.println();
 			}
 
+		}
+		System.out.println();
+	}
+
+	public static void printCalendar(int year, int month, String date) {
+		calendarOutline(year, month);
+		getLeapdays(year); // 윤년 계산
+		int week = getWeekday(year, month); // 1일 요일 계산
+		String[] dateSplit = date.split("-");
+		for (int i = 0; i < week; i++) {
+			System.out.printf("  \t");
+		}
+		if (year == Integer.parseInt(dateSplit[0]) && month == Integer.parseInt(dateSplit[1])) {
+			for (int i = 1; i <= MONTH_OF_MAX_DAYS[month - 1]; i++) {
+				if (i == Integer.parseInt(dateSplit[2])) {
+					System.out.printf(".");
+				}
+				System.out.printf(i + "\t");
+				if ((i + week) % 7 == 0) {
+					System.out.println();
+				}
+			}
+		}else {
+			for (int i = 1; i <= MONTH_OF_MAX_DAYS[month - 1]; i++) {
+				System.out.printf(i + "\t");
+				if ((i + week) % 7 == 0) {
+					System.out.println();
+				}
+
+			}
 		}
 		System.out.println();
 	}
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -6,74 +6,97 @@ import java.util.Calendar;
 
 public class Prompt {
 
-	static void ui() {
-		System.out.println("+--------------+");
-		System.out.println("| 1. 일정 등록");
-		System.out.println("| 2. 일정 검색");
-		System.out.println("| 3. 딜력 보기");
-		System.out.println("| 4. 도움말 q. 종료");
-		System.out.println("+--------------+");
-
-	}
-
-	static void scheduleRegistration() {
-
-	}
-
 	public static void main(String[] args) {
-		Calendar cal = Calendar.getInstance();
-		int year = cal.get(Calendar.YEAR);
-		int month = cal.get(Calendar.MONTH) + 1;
-		int command = 0;
-		String date;
-		String dateSchedule;
+		int year = 0;
+		int month = 0;
+		int[] temp = new int[2];
+		char command = 0;
+		String date = "0000-0-0";
 		HashMap<String, String> schedule = new HashMap<>();
 		Scanner scanner = new Scanner(System.in);
+		temp = getTodaysCalendar(year,month);
+		year = temp[0];
+		month = temp[1];
 		Calendar2.printCalendar(year, month);
-		ui();
-		while (true) {
-			System.out.println("명령 (1, 2, 3, 4, 5)");
+		cmdMenu();
+		boolean b = true;
+		while (b) {
+			System.out.println("명령 (1, 2, 3, h, q)");
 			System.out.print("> ");
-			command = scanner.nextInt();
+			command = scanner.next().charAt(0);
 			scanner.nextLine(); // 개행문자 제거
 			switch (command) {
-			case 1:	
-				System.out.println("[일정 등록] 날짜를 입력하세요.");
+			case '1':
+				System.out.println("[일정 등록] 날짜를 입력하세요.(yyyy.mm.dd)");
 				System.out.print("> ");
 				date = scanner.nextLine();
 				System.out.println("일정을 입력하세요.");
-				dateSchedule = scanner.nextLine();
+				String dateSchedule = scanner.nextLine();
 				schedule.put(date, dateSchedule);
 				System.out.println("일정이 등록되었습니다.");
 				break;
-			case 2:
-				System.out.println("[일정 검색] 날짜를 입력하세요.");
-				System.out.print("> ");
-				date = scanner.nextLine();
-				System.out.printf("%d개의 일정이 있습니다.\n", schedule.size());
-				for (int i = 0; i < schedule.size(); i++) {
-					System.out.printf("%d. %s\n", i+1, schedule.get(date));
-				}
+			case '2':
+				cmdSearch(schedule, scanner);
 				break;
-			case 3:
-				System.out.println("연도를 입력하세요.");
-				System.out.printf("YEAR> ");
-				year = scanner.nextInt();
-				System.out.println("월을 입력하세요.(종료 = -1)");
-				System.out.printf("MONTH> ");
-				month = scanner.nextInt();
-				Calendar2.printCalendar(year, month);
+			case '3':			
+				temp = cmdCal(year, month, scanner);
+				year = temp[0];
+				month = temp[1];
+				Calendar2.printCalendar(year, month, date);
 				break;
-			case 4:
-
+			case 'h':
+				cmdMenu();
 				break;
-			case 5:
-
+			case 'q':
+				b = false;
+				System.out.println("bye");
 				break;
-
 			default:
 				break;
 			}
 		}
+		scanner.close();
 	}
+
+	private static int[] getTodaysCalendar(int year, int month) {
+		int[] ym = new int[2];
+		Calendar cal = Calendar.getInstance();
+		year = cal.get(Calendar.YEAR);
+		ym[0] = year;
+		month = cal.get(Calendar.MONTH) + 1;
+		ym[1] = month;
+		return ym;
+		
+	}
+
+	private static void cmdMenu() {
+		System.out.println("+--------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 딜력 보기");
+		System.out.println("| h. 도움말 q. 종료");
+		System.out.println("+--------------+");
+
+	}
+
+	private static void cmdSearch(HashMap<String, String> schedule, Scanner s) {
+		System.out.println("[일정 검색] 날짜를 입력하세요.(yyyy.mm.dd)");
+		System.out.print("> ");
+		String date = s.nextLine();
+		System.out.printf("%s\n", schedule.get(date));
+	}
+
+	private static int[] cmdCal(int year, int month, Scanner s) {
+		int[] ym = new int[2];
+		System.out.println("연도를 입력하세요.");
+		System.out.printf("YEAR> ");
+		year = s.nextInt();
+		ym[0] = year;
+		System.out.println("월을 입력하세요.(종료 = -1)");
+		System.out.printf("MONTH> ");
+		month = s.nextInt();
+		ym[1] = month;
+		return ym;	
+	}
+
 }
